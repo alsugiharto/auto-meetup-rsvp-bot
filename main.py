64c25795d7cca5f6d835fc191b7eca8f4ffa6e34
al <a.k.sugiharto@gmail.com>
2019-07-14 12:07:09 +0200

    disable images loading

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ LINK_EVENT_LIST = 'https://www.meetup.com/{}/events/'.format(credentials.GROUP_N
 APP_NAME = 'meetupAutoRSVP'
 COOKIES_FILE_NAME = 'cookies.pkl'
 CONFIG_HIDE_PROCESS = True
+CONFIG_HIDE_IMAGES = True
 
 # =============================================
 # Code
@@ -43,6 +44,25 @@ def setup():
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--headless")
+
+    if CONFIG_HIDE_IMAGES:
+        sub_options = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2,
+                                                            'plugins': 2, 'popups': 2, 'geolocation': 2,
+                                                            'notifications': 2, 'auto_select_certificate': 2,
+                                                            'fullscreen': 2,
+                                                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2,
+                                                            'media_stream_mic': 2, 'media_stream_camera': 2,
+                                                            'protocol_handlers': 2,
+                                                            'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+                                                            'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                            'metro_switch_to_desktop': 2,
+                                                            'protected_media_identifier': 2, 'app_banner': 2,
+                                                            'site_engagement': 2,
+                                                            'durable_storage': 2}}
+        chrome_options.add_experimental_option('prefs', sub_options)
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--disable-extensions")
 
     # selenium INIT
     browser = webdriver.Chrome(options=chrome_options)
